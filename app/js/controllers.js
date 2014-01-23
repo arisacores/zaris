d2b448bf715e6059cbcbacc7714a76bd223306c2
arisacores <arisacores@gmail.com>
2014-01-23 09:50:04 -0100

    Added controlers and search

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,4 +8,15 @@ angular.module('myApp.controllers', []).
   }])
   .controller('MyCtrl2', [function() {
 
-  }]);
+  }])
+  .controller('MyCtrl3', [function() {
+
+  }])
+  .controller('ProcedimentosCtrl', function ($scope) {
+$scope.procedimentos = [
+	{'ambito': 'Indisponibilidade SI', 'situacao': 'Se não for possível resolver a questão: "Vá tomar um cafézinho e volte mais tarde."'},
+	{'ambito': 'Resolução sem despiste', 'situacao': 'Registar a chamada, venha outra!'},
+	{'ambito': 'Dificuldade em manter ligação', 'situacao': 'Rodar 3 vezes, se não, siga po team leader'}
+	];
+$scope.socool = "so cool";
+});
